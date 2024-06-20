23f6f0403e71e8a13617b73638f4d31c1fcd4c50
AguaMap <mapa.sa.agua@gmail.com>
2024-06-20 12:35:16 -0600

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,24 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
-  function createGallery(galleryId, images) {
-    const galleryDiv = document.getElementById(galleryId);
-    if (!galleryDiv) return;
+  function createCarousel(carouselId, images) {
+    const carouselDiv = document.getElementById(carouselId);
+    if (!carouselDiv) return;
 
-    let galleryContent = '<div class="row">';
+    let indicators = '';
+    let innerContent = '';
     images.forEach((image, index) => {
-      galleryContent += `
-        <div class="col-lg-3 col-md-4 col-6">
-          <div class="d-block mb-4 h-100">
-            <img class="img-fluid img-thumbnail" src="${image}" alt="Image ${index + 1}">
-          </div>
+      indicators += `
+        <button type="button" data-bs-target="#${carouselId}" data-bs-slide-to="${index}" ${index === 0 ? 'class="active"' : ''} aria-current="${index === 0 ? 'true' : 'false'}"></button>`;
+      
+      innerContent += `
+        <div class="carousel-item ${index === 0 ? 'active' : ''}">
+          <img src="${image}" class="d-block w-100" alt="Image ${index + 1}">
         </div>`;
     });
-    galleryContent += '</div>';
 
-    galleryDiv.innerHTML = galleryContent;
+    carouselDiv.innerHTML = `
+      <div id="${carouselId}" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          ${indicators}
+        </div>
+        <div class="carousel-inner">
+          ${innerContent}
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#${carouselId}" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#${carouselId}" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>`;
   }
 
   const galleries = {
-    'gallery-texcoco': [
+    'carousel-texcoco': [
       'NodoTexcoco/NODOTEXCOCO001.jpg',
       'NodoTexcoco/NODOTEXCOCO002.jpg',
       'NodoTexcoco/NODOTEXCOCO003.jpg',
@@ -30,7 +47,8 @@ document.addEventListener('DOMContentLoaded', function () {
     ]
   };
 
-  for (const [galleryId, images] of Object.entries(galleries)) {
-    createGallery(galleryId, images);
+  for (const [carouselId, images] of Object.entries(galleries)) {
+    createCarousel(carouselId, images);
   }
 });
+
