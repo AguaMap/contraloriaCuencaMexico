925fb15f12d67958bc291df8489247d28c9b8514
AguaMap <mapa.sa.agua@gmail.com>
2024-07-16 08:10:09 -0600

    Update mapa_interactivo.js

--- mapa_interactivo.js
+++ mapa_interactivo.js
@@ -17,7 +17,7 @@ function toggleAddMarker() {
         document.getElementById('add-marker-btn').innerText = 'Click on the map to add marker';
     } else {
         map.off('click', onMapClick);
-        document.getElementById('add-marker-btn').innerText = 'Add Marker';
+        document.getElementById('add-marker-btn').innerText = 'Agregar comentario';
     }
 }
 
@@ -27,14 +27,14 @@ function onMapClick(e) {
 
     var popupContent = `
         <div>
-            <h3>Add Details</h3>
-            <label for="comment">Comment:</label><br>
+            <h3>Agregar detalles</h3>
+            <label for="comment">Comentario:</label><br>
             <input type="text" id="comment" name="comment"><br>
-            <label for="photo">Photo URL:</label><br>
+            <label for="photo">URL Foto:</label><br>
             <input type="text" id="photo" name="photo"><br>
-            <label for="video">Video URL:</label><br>
+            <label for="video">URL Vídeo:</label><br>
             <input type="text" id="video" name="video"><br>
-            <button onclick="saveDetails(this, ${marker._leaflet_id})">Save</button>
+            <button onclick="saveDetails(this, ${marker._leaflet_id})">Guardar</button>
         </div>
     `;
     marker.bindPopup(popupContent).openPopup();
@@ -48,10 +48,10 @@ window.saveDetails = function(button, markerId) {
     var photo = button.parentNode.querySelector('#photo').value;
     var video = button.parentNode.querySelector('#video').value;
 
-    var detailsContent = `<p><b>Comment:</b> ${comment}</p>`;
+    var detailsContent = `<p><b>Comentario:</b> ${comment}</p>`;
     if (photo) detailsContent += `<p><b>Photo:</b> <img src="${photo}" alt="Photo" width="100"></p>`;
     if (video) detailsContent += `<p><b>Video:</b> <a href="${video}" target="_blank">Watch Video</a></p>`;
-    detailsContent += `<button onclick="addComment(${markerId})">Add Comment</button>`;
+    detailsContent += `<button onclick="addComment(${markerId})">Agregar comentario</button>`;
 
     var marker = map._layers[markerId];
     marker.bindPopup(detailsContent).openPopup();
@@ -68,10 +68,10 @@ window.addComment = function(markerId) {
 
     var addCommentPopupContent = `
         <div>
-            <h3>Add Comment</h3>
-            <label for="new-comment">New Comment:</label><br>
+            <h3>Agregar comentario</h3>
+            <label for="new-comment">Nuevo comentario:</label><br>
             <input type="text" id="new-comment" name="new-comment"><br>
-            <button onclick="saveComment(this, ${markerId})">Save Comment</button>
+            <button onclick="saveComment(this, ${markerId})">Guardar comentario</button>
         </div>
     `;
     marker.bindPopup(addCommentPopupContent).openPopup();
@@ -91,12 +91,12 @@ window.saveComment = function(button, markerId) {
     var videoHTML = tempDiv.querySelector('p:nth-child(3)') ? tempDiv.querySelector('p:nth-child(3)').outerHTML : '';
     var previousComments = tempDiv.querySelectorAll('p.comment') ? Array.from(tempDiv.querySelectorAll('p.comment')).map(p => p.outerHTML).join('') : '';
 
-    var updatedContent = `<p><b>Comment:</b> ${originalComment}</p>`;
+    var updatedContent = `<p><b>Comentario:</b> ${originalComment}</p>`;
     if (photoHTML) updatedContent += photoHTML;
     if (videoHTML) updatedContent += videoHTML;
     updatedContent += previousComments;
-    updatedContent += `<p class="comment"><b>Additional Comment:</b> ${newComment}</p>`;
-    updatedContent += `<button onclick="addComment(${markerId})">Add Comment</button>`;
+    updatedContent += `<p class="comment"><b>Comentario adicional:</b> ${newComment}</p>`;
+    updatedContent += `<button onclick="addComment(${markerId})">Agregar comentario</button>`;
 
     marker.bindPopup(updatedContent).openPopup();
 };
